158fa322cc28e1dd18027c0b19e61e1d715d5c90
Hassan Saud <hm862333@gmail.com>
2022-02-18 00:55:21 +0500

    Add files via upload

---
+++ miniChild.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include  <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+#define COMMAND_LENGTH 50
+#define MESSAGE_LENGTH 1234
+
+
+int location(const char* cmd){
+	int l1;
+	for(int num=0; num<strlen(cmd); num++){
+		if(isspace(cmd[num])) l1=num;
+		while(isspace(cmd[num]) && num<(strlen(cmd)-1)){
+			num+=1;
+			if(!(isspace(cmd[num]))) l1=-1;
+		}
+	}
+	return l1;
+}
+
+int main(int num, char* argv[]){
+	int mainSocket,  mainPort=htons(12345),  returnStatus=0;
+	struct sockaddr_in  mainAddressStructure;
+	char path[]=" > /root/commandResult"  ;
+	bzero(&mainAddressStructure,  sizeof(mainAddressStructure));
+	mainAddressStructure.sin_family=AF_INET;
+	inet_pton(AF_INET,  argv[1]  ,  &mainAddressStructure.sin_addr.s_addr);
+	mainAddressStructure.sin_port=mainPort;
+	mainSocket = socket(AF_INET,  SOCK_STREAM, IPPROTO_TCP);
+	if(mainSocket<0){
+		printf("\n%s\n", "Can't open Socket");
+		exit (1);
+	}
+	returnStatus=connect(mainSocket,  (struct sockaddr*) &mainAddressStructure,  sizeof(mainAddressStructure));
+	if(returnStatus==0){
+		printf("\n%s\n", "Connection Successfull.");
+	}
+	else{
+		printf("\n%s\n",  "Can't connect to Socket.");
+		close (mainSocket);
+		exit (1);
+	}
+	while(1){
+	char command[COMMAND_LENGTH]=" ", message[MESSAGE_LENGTH]=" ";
+	
+	returnStatus=read(mainSocket,  command,  COMMAND_LENGTH);
+	if(returnStatus>0){
+		printf("\nCommand RECEIVED: %s\n", command);
+		strcat(command,path);
+		
+		
+	}
+	else if(returnStatus < 0){
+		printf("\n%s\n", "can't read from connection.");
+		printf("\nBuffer contains:  %s\n", command );
+		close (mainSocket);
+		exit (1);
+	}
+		int size=strlen(command);
+		char modifiedCommand[size];
+		strncpy(modifiedCommand, command, size);
+		modifiedCommand[size]='\0';
+		printf("\nCommand Converted to: %s\n", modifiedCommand);
+		printf("\nNow actual Command becomes: %\n", modifiedCommand);
+		system(modifiedCommand);
+		int  fd; if((fd=open("/root/commandResult", O_RDONLY )) < 0){
+				printf("\n%s\n","Can't open File specified.");
+				close(mainSocket);
+				exit (1);
+			}
+		int bytesRead; if((bytesRead=read(fd,message, MESSAGE_LENGTH)) < 0 ){
+			printf("\n%s\n", "Can't read from File");
+			close(mainSocket);
+			close(fd);
+			exit(1);
+			}
+		int bytesWritten=write(mainSocket, message, MESSAGE_LENGTH);
+		if(bytesWritten < 0){printf("\n%s\n", "Can't Write back  to  Server Connection specified. ");
+			close(mainSocket);
+			close(fd);
+			exit(1);
+		}
+		printf("\n%s\n%s\n", "Command Result Sent: ", message);
+		//for(int num=0;num<COMMAND_LENGTH;num++)command[num]=' ';
+		//for(int num=0; num<MESSAGE_LENGTH; num++)message[num]=' ';
+	}
+
+	close (mainSocket);
+	return 0;
+
+
+}
---
+++ miniServer.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <wait.h>
+#include <ctype.h>
+#include  <string.h>
+
+#define COMMAND_LENGTH 50
+#define MESSAGE_LENGTH 1234
+
+int location(const char* cmd){
+	int l1;
+	for(int num=0; num<strlen(cmd); num++){
+			if(  isspace(cmd[num])  || isblank(cmd[num])) l1=num;
+			while(  (isspace(cmd[num])  || isblank(cmd[num]))   && num<(strlen(cmd)-1)){
+				num+=1;
+				if(  !( isspace(cmd[num]) || isblank(cmd[num]) )  ) l1=-1;
+			}
+		}
+	return l1;
+}
+int main(int num,  char* argv[]){
+	
+	int mainSocket=0,  childSocket=0,  mainPort=htons(12345),  returnStatus=0;
+	struct sockaddr_in mainAddressStructure;
+	struct sockaddr_in childAddressStructure;
+	int childNameLength=0;
+	mainSocket=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(mainSocket<0) 
+		{  printf("\n%s\n","ERROR:: Couldn't initiate SOCKET.");  exit (1);  }
+	else
+		{  printf("\n%s\n","Socket is initialized properly.");  }
+	bzero(&mainAddressStructure,sizeof(mainAddressStructure));
+	mainAddressStructure.sin_family=AF_INET;
+	mainAddressStructure.sin_addr.s_addr=htonl(INADDR_ANY);
+	mainAddressStructure.sin_port=mainPort;
+	returnStatus=bind(mainSocket,  (struct sockaddr*) &mainAddressStructure,  sizeof(mainAddressStructure));
+	if(returnStatus==0) { 
+		printf("\n%s\n","Bind Complete"); }
+	else 
+		{  printf("\n%s\n","Can't bind properly."); 
+			 close (mainSocket); 
+			 exit (1);  }
+	returnStatus=listen(mainSocket,5);
+	if(returnStatus==-1)
+		{  printf("\n%s\n","Can't listen on this socket.");  close (mainSocket);  exit (1);  }
+	while(1)  {
+		
+		childSocket = accept(mainSocket,  (struct sockaddr*) &childAddressStructure,  &childNameLength);
+		if(childSocket==-1)  {
+					printf("\n%s\n","Can't accept Connections.");
+					close(mainSocket);
+					exit (1);
+					return 1;}
+		else if(childSocket>1){
+			printf("\n%s\n", "Connected Successfully."); }
+			printf("\n%s\n","Connected with the Client.");
+			 while(1){
+			char message[MESSAGE_LENGTH]=" ", command[COMMAND_LENGTH]=" ";
+			printf("\nInitially Command contains: %s\n", command);
+			printf("%s","Enter Command to execute: ");
+			fgets(command,COMMAND_LENGTH-2,stdin);
+			printf("\nCommand SENT: %s \n", command );
+			int index=location(command);
+			char modifiedCommand[index+1];
+			strncpy(modifiedCommand, command, index);
+			printf("\nModified Command becomes: %s\n", modifiedCommand); //remove this
+			modifiedCommand[index]='\0';
+			int bytesWritten = -1;	
+			if((bytesWritten=write(childSocket,modifiedCommand,strlen(modifiedCommand)))<strlen(modifiedCommand)){
+					printf("\n%s\n","Can't Write  COMMAND to Child connection specified.");
+					close(childSocket);
+					close(mainSocket);
+					exit(1);
+				}
+			int bytesRead=-1;	if((bytesRead=read(childSocket,message,MESSAGE_LENGTH)) < 0){
+					printf("\n%s\n","Can't Read The results from The Child Connection.");
+					close(childSocket);
+					close(mainSocket);
+					exit(1);
+				}
+			printf("\n\nCommand Output: \n%s\n",  message);
+			}
+			close(childSocket);
+	}
+	close (mainSocket);
+	return 0;
+
+
+
+
+
+}
